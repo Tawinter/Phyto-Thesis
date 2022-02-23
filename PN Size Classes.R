50acef13578f6d5a245affa6aa4325b33933c020
tawinter <tiffany.winter@unh.edu>
2022-02-23 10:58:43 -0500

    finished creating size class graphs and updated word doc

--- PN Size Classes.R
+++ PN Size Classes.R
@@ -3,7 +3,11 @@ library(tinytex)
 library(dplyr)
 library(tidyr)
 library(formattable)
+library(forcats)
 
+install.packages("forcats")
+
+setwd("D:/R/phyto-thesis")
 
 hampton <- read.csv("R_HHHR2.csv" , stringsAsFactors = TRUE)
 cml <- read.csv("R_UNH_Pier.csv", stringsAsFactors = TRUE)
@@ -36,9 +40,47 @@ size_cml_1 <- gather(size_cml_1, Class, Sum_Abundance, Large_PN:Small_PN, factor
 size_hampton_1 <- gather(size_hampton_1, Class, Sum_Abundance, Large_PN:Small_PN, 
                          factor_key=TRUE)
 
-size_cml_1$MY <- paste(size_cml_1$Month, "-", size_cml_1$Year)
+#Convert month number to month 3 letter abbreviation
+mymonths <- c("Jan","Feb","Mar",
+              "Apr","May","Jun",
+              "Jul","Aug","Sep",
+              "Oct","Nov","Dec")
 
-#Adding in percentages
+size_cml_1$MoAb <- mymonths[ size_cml_1$Month ]
+
+size_hampton_1$MoAb <- mymonths[ size_hampton_1$Month ]
+
+#Combining month and year
+
+size_cml_1$MY <- paste(size_cml_1$MoAb, "-", size_cml_1$Year)
+
+size_hampton_1$MY <- paste(size_hampton_1$MoAb, "-", size_hampton_1$Year)
+
+#Graphing each month in each year separately on one graph
+
+ggplot(size_cml_1[which(size_cml_1$Sum_Abundance > 0.0),], aes(x = fct_inorder(MY), y = Sum_Abundance, fill = Class)) +
+  geom_col(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_brewer(palette = "Pastel1") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = "Month", y = "Percentage of Abundance")
+
+ggplot(size_hampton_1[which(size_hampton_1$Sum_Abundance > 0.0),], aes(x = fct_inorder(MY), y = Sum_Abundance, fill = Class)) +
+  geom_col(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_brewer(palette = "Pastel1") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = "Month", y = "Percentage of Abundance")
+
+
+
+
+
+
+
+#(RETIRED) Adding in percentages
 size_cml_pct <- size_cml_1 %>%
   group_by(Year, Month) %>%
   mutate(freq = formattable::percent(Sum_Abundance / sum(Sum_Abundance)))
@@ -47,7 +89,7 @@ size_hampton_pct <- size_hampton_1 %>%
   group_by(Year, Month) %>%
   mutate(freq = formattable::percent(Sum_Abundance / sum(Sum_Abundance)))
 
-#Fill Graphs of all
+#(RETIRED) Fill Graphs of all
 ggplot(size_cml_1, aes(x = Month, y = Sum_Abundance, fill = Class)) +
   geom_col(position = "fill") +
   scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12),
@@ -66,8 +108,7 @@ ggplot(size_hampton_1, aes(x = Month, y = Sum_Abundance, fill = Class)) +
   theme_classic() +
   labs(x = "Month", y = "Percentage of Abundance")
 
-
-##Graphs: Years on separate graphs with percentages
+#(RETIRED) Graphs: Years on separate graphs with percentages
 
 ggplot(size_cml_pct[which(size_cml_pct$Year== 2017),], aes(x = Month, y = Sum_Abundance, fill = Class)) +
   geom_col(position = "fill") +
